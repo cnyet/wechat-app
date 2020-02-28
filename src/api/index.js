33d7a02ec1d6519faee2e6bd5bfc18ea71b05751
cnyet <yateup@gmail.com>
2020-02-28 14:07:24 +0800

    feat: add wechat-share

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,8 @@ export default {
   wechatRedirect: '/api/wechat/redirect?url=http%3A%2F%2Fm.yates.com&scope=snsapi_userinfo',
   wechatConfig: '/api/wechat/jssdk',
   getUserInfo: '/api/wechat/getUserInfo',
+  getBaseToken: '/api/wechat/getBaseToken',
   payWallet: '/api/wechat/paywallet',
   test: 'http://rap2.taobao.org:38080/app/mock/1484/visiters',
+  testImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582870103522&di=1302d808e45275bc08390c6840f29bd2&imgtype=0&src=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F40500000%2Fnewclubimage-despicable-me-minions-40532353-5038-4325.jpg'
 };
--- src/views/index.vue
+++ src/views/index.vue
@@ -5,10 +5,19 @@
       <p class="userName">{{userName}}</p>
     </header>
     <footer class="footer">
-      <a href="javascript:;" class="weui-btn weui-btn_primary">分享</a>
+      <a href="javascript:;" class="weui-btn weui-btn_primary" @click="wechatShare">分享</a>
       <a href="javascript:;" class="weui-btn weui-btn_warn">体验</a>
       <a href="javascript:;" class="weui-btn weui-btn_default">活动详情</a>
     </footer>
+    <!--BEGIN toast-->
+    <div v-show="showTips">
+        <div class="weui-mask_transparent"></div>
+        <div class="weui-toast">
+            <i class="weui-icon-success-no-circle weui-icon_toast"></i>
+            <p class="weui-toast__content">已完成</p>
+        </div>
+    </div>
+    <!--end toast-->
   </div>
 </template>
 <script>
@@ -23,15 +32,12 @@ export default {
   data: function() {
     return {
       userName: '',
-      avatar: ''
+      avatar: '',
+      showTips: false
     };
   },
   mounted() {
     this.checkuserAuth();
-    // const APPID = CONFIG.APPID;
-    // const REDIRECT_URI = CONFIG.REDIRECT_URI;
-    // const SCOPE = CONFIG.SCOPE;
-    // location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${APPID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}&state=STATE#wechat_redirect`;
   },
   methods: {
     checkuserAuth() {
@@ -72,14 +78,57 @@ export default {
       }).catch(err => {
         console.log(err);
       });
+    },
+    wechatShare() {
+      // 获取普通access_token
+      httpRequest.getByParam(API.getBaseToken).then(res =>{
+        console.log(res);
+        if (res) {
+          wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: res.appId, // 必填，公众号的唯一标识
+            timestamp: res.timestamp, // 必填，生成签名的时间戳
+            nonceStr: res.nonceStr, // 必填，生成签名的随机串
+            signature: res.signature,// 必填，签名
+            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone'] // 必填，需要使用的JS接口列表
+          });
+          this.showTips = true;
+          setTimeout(()=>{
+            this.showTips = false;
+          },1000);
+          this.setShareContent();
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    setShareContent() {
+      wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+        wx.onMenuShareTimeline({
+          title: '测试', // 分享标题
+          desc: '测试文本', // 分享描述
+          link: 'http://m.imooc.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: API.testImg, // 分享图标
+          success: function (res) {
+            console.log(res);
+          }
+        });
+        wx.onMenuShareAppMessage({
+          title: '测试', // 分享标题
+          desc: '测试文本', // 分享描述
+          link: 'http://m.imooc.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: API.testImg, // 分享图标
+          success: function (res) {
+            console.log(res);
+          }
+        });
+      });
     }
   }
 };
-
 </script>
 <style lang="scss">
 @import "../assets/styles/mixin.scss";
-
 .home {
   display: flex;
   flex-direction: column;
@@ -108,5 +157,4 @@ export default {
     align-items: center;
   }
 }
-
 </style>
